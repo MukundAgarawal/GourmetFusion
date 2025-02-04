7750f7a37e4b4ce5127cc04fd09ea71337c9ed35
MukundAgarawal <mukundagarawal9@gmail.com>
2025-02-04 14:26:21 +0530

    added frontend folder

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = function(){
+    return(
+        <div>
+            <img className="w-80" src="https://supermarketitaly.com/cdn/shop/collections/fusion-gourmet-684538.png?v=1683140621" alt="" />
+        </div>
+    )
+}
+
+export default Navbar;
